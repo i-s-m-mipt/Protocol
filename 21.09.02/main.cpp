82ab6daa5f8e4e438c0419642f7aa0eed4600cac
Ivan Makarov <i.s.m.mipt@yandex.ru>
2021-09-02 23:28:09 +0300

    Черновик переработан, создана заготовка для системы управления.

--- 21.09.02/main.cpp
+++ 21.09.02/main.cpp
@@ -1,93 +1,209 @@
+#include <cstdlib>
+#include <initializer_list>
 #include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
 #include <boost/asio.hpp>
 
-void write_data(boost::asio::ip::tcp::socket & socket, std::string comand)
+namespace solution
 {
-	boost::asio::write(socket, boost::asio::buffer(comand));
-}
-
-struct LAN_COMMAND_0
-{
-	uint8_t XOR_SUM = 0x00;
-	uint8_t Ver = 0x02;
-	uint8_t CMD_TYPE = 0x00;
-	uint8_t CMD_IDENTIFICATION = 0x00;
-	uint16_t LENGTH_DATA = 0x00;
-	uint8_t DATA[0];
-};
-
-uint8_t xor_sum(uint8_t * data, uint16_t length)
-{
-	uint8_t xor_temp = 0xFF;
-	while (length--) { xor_temp += *data; data++; }
-	return(xor_temp ^ 0xFF);
-}
-
-
-int main()
-{
-	LAN_COMMAND_0 lan_command;
-
-	lan_command.XOR_SUM = xor_sum((uint8_t *)&lan_command.XOR_SUM, sizeof(lan_command));
-
-	std::string raw_ip_address_1 = "192.168.1.2";
-	std::string raw_ip_address_2 = "192.168.1.3";
-	auto port = 5000;
-
-	boost::asio::ip::tcp::endpoint endpoint_1(boost::asio::ip::address::from_string(raw_ip_address_1), port);
-	boost::asio::ip::tcp::endpoint endpoint_2(boost::asio::ip::address::from_string(raw_ip_address_2), port);
-
-	boost::asio::io_service io_service;
-
-	boost::asio::ip::tcp::socket socket_1(io_service, endpoint_1.protocol());
-	socket_1.connect(endpoint_1);
-
-	std::cout << "192.168.1.2 connected" << std::endl;
-
-	boost::asio::ip::tcp::socket socket_2(io_service, endpoint_2.protocol());
-	socket_2.connect(endpoint_2);
-
-	std::cout << "192.168.1.3 connected" << std::endl;
-
-	//boost::asio::write(socket_1, boost::asio::buffer(&lan_command, sizeof(lan_command)));
-
-	//boost::asio::write(socket_2, boost::asio::buffer(&lan_command, sizeof(lan_command)));
-
-	//std::cout << "lan_command_0 write" << std::endl;
-
-	/*const std::size_t size = 30;
-
-	boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::address_v4::any(), port);
-	boost::asio::ip::tcp::acceptor acceptor(io_service, endpoint.protocol());
-	acceptor.bind(endpoint);
-	acceptor.listen(size);
-	boost::asio::ip::tcp::socket socket(io_service);
-	acceptor.accept(socket);*/
-
-	int a = 0;
-
-	while (true)
+	class Controller
 	{
-		std::cin >> a;
+	public:
 
-		if (a == 0)
+		using endpoint_t = boost::asio::ip::tcp::endpoint;
+
+		using ip_address_t = boost::asio::ip::address;
+
+		using socket_t = boost::asio::ip::tcp::socket;
+
+		using sockets_container_t = std::vector < socket_t > ;
+
+	private:
+
+		struct Command
 		{
-			std::string comand = ""; //остановка
+			std::uint8_t  control_sum      = 0x00;
+			std::uint8_t  protocol_version = 0x02; // default
+			std::uint8_t  command_type     = 0x00;
+			std::uint8_t  command_id       = 0x00;
+			std::uint16_t data_length      = 0x00;
+
+			std::uint8_t  data[1024] = {}; // max 1024
+		};
+
+	public:
+
+		explicit Controller(const std::initializer_list < 
+			std::pair < std::string, unsigned int > > & endpoints)
+		{
+			m_sockets.reserve(std::size(endpoints));
+
+			for (const auto & [ip_address, port] : endpoints)
+			{
+				endpoint_t enpoint(ip_address_t::from_string(ip_address), port);
+
+				socket_t socket(m_io_service, enpoint.protocol());
+
+				socket.connect(enpoint);
+
+				std::cout << "Socket on [" << ip_address << ":" << port <<
+					"] successfully created" << std::endl;
+
+				Command command;
+
+				send_command(command, socket);
+
+				command.data_length = 0x08;
+
+				command.data[0] = 0xEF;
+				command.data[1] = 0xCD;
+				command.data[2] = 0xAB;
+				command.data[3] = 0x89;
+				command.data[4] = 0x67;
+				command.data[5] = 0x45;
+				command.data[6] = 0x23;
+				command.data[7] = 0x01;
+
+				send_command(command, socket);
+
+				std::cout << "Controller on [" << ip_address << ":" << port <<
+					"] ready to work" << std::endl;
+
+				m_sockets.push_back(std::move(socket));
+			}
+		}
+
+		~Controller() noexcept = default;
+
+	public:
+
+		void move_forward()
+		{
 
 		}
 
-		if (a == 1)
+		void move_backward()
 		{
-			std::string comand = ""; //крутить вправо
-			write_data(socket_1, comand);
+
 		}
 
-		if (a == 2)
+		void stop()
 		{
-			std::string comand = ""; //крутить влево
-			write_data(socket_1, comand);
+
 		}
+
+	private:
+
+		void send_command(Command & command, const socket_t & socket)
+		{
+			update_control_sum(command);
+
+			boost::asio::write(socket, boost::asio::buffer(
+				&command, sizeof(command)));
+		}
+
+		void update_control_sum(Command & command)
+		{
+			command.control_sum = get_control_sum(
+				(uint8_t *)&command.control_sum, sizeof(command));
+		}
+
+		std::uint8_t get_control_sum(std::uint8_t * data, std::uint16_t length)
+		{
+			std::uint8_t s = 0xFF;
+
+			while (length--) 
+			{ 
+				s += *(data++);
+			}
+
+			return (s ^ 0xFF);
+		}
+
+	public:
+
+		const auto & sockets() const noexcept
+		{
+			return m_sockets;
+		}
+
+	private:
+
+		boost::asio::io_service m_io_service;
+
+		sockets_container_t m_sockets;
+	};
+
+} // namespace solution
+
+using Controller = solution::Controller;
+
+int main(int argc, char ** argv)
+{
+	try
+	{
+		{
+			Controller controller({
+				{ "192.168.1.2", 5000U}, 
+				{ "192.168.1.3", 5000U} });
+
+			char command = ' ';
+
+			while (command != 'e' && command != 'E')
+			{
+				std::cout << "Enter command (B - backward, S - stop, F - forward, E - exit) : ";
+
+				std::cin >> command;
+
+				switch (command)
+				{
+				case 'b': case 'B':
+				{
+					controller.move_backward();
+
+					break;
+				}
+				case 's': case 'S':
+				{
+					controller.stop();
+
+					break;
+				}
+				case 'f': case 'F':
+				{
+					controller.move_forward();
+
+					break;
+				}
+				default:
+				{
+					break;
+				}
+				}
+			}
+		}
+
+		system("pause");
+
+		return EXIT_SUCCESS;
 	}
-	system("pause");
-	return 0;
+	catch (const std::exception & exception)
+	{
+		std::cerr << exception.what() << std::endl;
+
+		system("pause");
+
+		return EXIT_FAILURE;
+	}
+	catch (...)
+	{
+		std::cerr << "unknown exception" << std::endl;
+
+		system("pause");
+
+		return EXIT_FAILURE;
+	}
 }
